62e1392da1daaa97d1706ec4354fab9257a9dc11
Jaskaran Kaur <jasdhanesar111@gmail.com>
2023-11-17 23:54:18 -0500

    Uploaded Project

---
+++ SchoolProject/Controllers/TeacherDataController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SchoolProject.Models;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace SchoolProject.Controllers
+{
+    public class TeacherDataController : ApiController
+    {
+        // GET: TeacherData
+        // The database context class which allows us to access our MySQL Database.
+        private SchoolDbContext School = new SchoolDbContext();
+
+        //This Controller Will access the teachers table of our school database.
+        /// <summary>
+        /// Returns a list of Teachers in the system
+        /// </summary>
+        /// <example>GET api/TeacherData/ListTeachers</example>
+        /// <returns>
+        /// A list of teacher objects.
+        /// </returns>
+        [HttpGet]
+        [Route("api/TeacherData/ListTeachers/{SearchKey?}")]
+        public IEnumerable<Teacher> ListTeachers(string SearchKey = null)
+        {
+            //Create an instance of a connection
+            MySqlConnection Conn = School.AccessDatabase();
+
+            //Open the connection between the web server and database
+            Conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "Select * from Teachers where lower(teacherfname) like lower(@key) or lower(teacherlname) like lower(@key) or lower(concat(teacherfname, ' ', teacherlname)) like lower(@key)";
+
+            cmd.Parameters.AddWithValue("@key", "%" + SearchKey + "%");
+            cmd.Prepare();
+
+            //Gather Result Set of Query into a variable
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            //Create an empty list of Teachers
+            List<Teacher> Teachers = new List<Teacher>{};
+
+            //Loop Through Each Row the Result Set
+            while (ResultSet.Read())
+            {
+                //Access Column information by the DB column name as an index
+                int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                string TeacherFname = ResultSet["teacherfname"].ToString();
+                string TeacherLname = ResultSet["teacherlname"].ToString();
+                string HireDate = ResultSet["hiredate"].ToString();
+                string Salary = ResultSet["salary"].ToString();
+
+                Teacher NewTeacher = new Teacher();
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.HireDate = HireDate;
+                NewTeacher.Salary = Salary;
+
+                //Add the Teacher Name to the List
+                Teachers.Add(NewTeacher);
+            }
+
+            //Close the connection between the MySQL Database and the WebServer
+            Conn.Close();
+
+            //Return the final list of teacher names
+            return Teachers;
+        }
+
+
+        /// <summary>
+        /// Returns an individual author from the database by specifying the primary key authorid
+        /// </summary>
+        /// <param name="id">the teacher's id in the database</param>
+        /// <returns>A teacher object</returns>
+        [HttpGet]
+        public Teacher FindTeacher(int id)
+        {
+            Teacher NewTeacher = new Teacher();
+
+            //Create an instance of a connection
+            MySqlConnection Conn = School.AccessDatabase();
+
+            //Open the connection between the web server and database
+            Conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "Select * from Teachers where teacherid= @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+
+            //Gather Result Set of Query into a variable
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            while (ResultSet.Read())
+            {
+                //Access Column information by the DB column name as an index
+                int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                string TeacherFname = ResultSet["teacherfname"].ToString();
+                string TeacherLname = ResultSet["teacherlname"].ToString();
+                string HireDate = ResultSet["hiredate"].ToString();
+                string Salary = ResultSet["salary"].ToString();
+
+                
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.HireDate = HireDate;
+                NewTeacher.Salary = Salary;
+            }
+
+
+            return NewTeacher;
+        }
+    }
+}
